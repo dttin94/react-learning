de2b778d729bef2d75888efa84a3d2e9106f79be
nitduong <insecure10@gmail.com>
2017-03-16 19:30:53 +0700

    Use ES6 arrow function and constructor, state

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -22,12 +21,21 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        { list.map(function(item) {
+        { this.state.list.map(item => {
           return (
-            <div>
+            <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
               </span>
