63b32ba79c0d9086eceaa92a16eda48b216dd244
Thien Tin <dttin94@gmail.com>
2017-03-18 12:17:18 +0700

    Thay doi cau truc component

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -48,29 +48,105 @@ class App extends Component {
   }
 
   render() {
+    const {list, searchTerm } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text" onChange={this.onSearchChange}/>
-        </form>
-        { this.state.list.filter(isSearched(this.state.searchTerm)).map(item => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
-              </span>
-            </div>
-          );
-        })}
+        <Search value={searchTerm} onChange={this.onSearchChange}/>
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
 }
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+
+    return (
+      <form>
+        <input type="text" value={searchTerm} onChange={this.onSearchChange}/>
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      { list.filter(isSearched(searchTerm)).map(item => {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
+            </span>
+          </div>
+          );
+      })}
+    )
+  }
+}
+
+
+/*const items = []
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialItems: [
+        "Apples",
+        "Broccoli",
+        "Chicken",
+        "Duck",
+        "Eggs",
+        "Fish",
+        "Granola",
+        "Hash Browns"
+      ],
+      items
+    }
+
+    this.filterList = this.filterList.bind(this)
+  }
+
+  componentWillMount() {
+    this.setState({ items: this.state.initialItems });
+  }
+
+  filterList(event) {
+    const updatedList = this.state.initialItems.filter(
+      (item) => item.toLowerCase().includes(event.target.value.toLowerCase()));
+    this.setState({ items: updatedList });
+  }
+
+  render () {
+    const { items } = this.state;
+
+    return (
+      <div className="filter-list">
+        <input type="text" placeholder="Search" onChange={this.filterList}/>
+        <List items={items}/>
+      </div>
+    );
+  }
+}
+
+class List extends Component {
+  render () {
+    return (
+      <ul>
+        { this.props.items.map(item => <li key={item}>{item}</li>) }
+      </ul>
+    )
+  }
+}*/
 
 export default App;
